bf28047d2adece388a795e072cd2535c6b364b5a
Vikash kumar <Vraj67207@gmail.com>
2024-05-17 12:21:02 +0530

    "update"

--- app/src/main/java/com/example/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabase/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.roomdatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var dataBase: ContactDataBase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        dataBase = Room.databaseBuilder(applicationContext,
+        ContactDataBase::class.java,
+        "contactDB").build()
+        GlobalScope.launch {
+            dataBase.contactDao().insertContact(Contact(0,"viaksh","564437567"))
+            dataBase.contactDao().insertContact(Contact(0,"jon","564437567"))
+
+
+        }
+
+
     }
 }
